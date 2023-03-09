a13e1ff00f4f511b30bae7543fa12b35f54e71be
Rıdvan AYDIN <aydinridvan2834@gmail.com>
2023-03-09 12:10:06 +0300

    useEffect 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [count, setCount] = useState(0);
-  function decermement() {
-    setCount(prevCount => prevCount - 1);
-  }
-  function increment() {
-    setCount(prevCount => prevCount + 1);
-  }
+  const [resources, setResources] = useState('posts')
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${resources}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  },[resources])
 
   return (
     <div className="App">
-      <button onClick={decermement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
+      <div>
+      <button onClick={()=>setResources('posts')}>Post</button>
+      <button onClick={()=>setResources('users')}>Users</button>
+      <button onClick={()=>setResources('comments')}>Comments</button>
+      <h1>{resources}</h1>
+      </div>
+      <div>
+      {items.map(item=>{
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
+      </div>
+
     </div>
   );
 }
